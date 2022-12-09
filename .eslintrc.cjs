7ffde79e2707791bb103d1598382a8cd46d25a14
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2022-12-09 10:59:51 +0100

    chore: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+  ],
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      ],
+      'extends': [
+        'plugin:cypress/recommended',
+      ],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    curly: ['warn', 'all'],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'never'],
+    indent: ['warn', 2, { SwitchCase: 1 }],
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NsButton from './components/ns-button/NsButton.vue'
+</script>
+
+<template>
+  <main>
+    <ns-button>test</ns-button>
+  </main>
+</template>
