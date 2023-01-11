ab737e7ae9528db6a5703e6c1324beda31bc3c17
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-01-11 20:01:09 +0100

    feat: add input details components to display a message or/and a counter

---
+++ src/components/pn-input-detail/PnInputDetail.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps({
+  status: {
+    type: String,
+    default: '',
+  },
+  message: {
+    type: String,
+    default: '',
+  },
+  counter: {
+    type: Number,
+    default: 0,
+  },
+})
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PnInputDetail',
+  computed: {
+    statusClass() {
+      if (!this.status) {
+        return;
+      }
+
+      return `text-${this.status}`
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="input-detail">
+    <span :class="['input-message', 'flex-auto', statusClass]">{{ message }}</span>
+    <span class="input-counter flex-initial">{{ counter }}</span>
+  </div>
+</template>
