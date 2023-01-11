8641fb0b8851056a64d986a91b98be6515535970
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-01-11 18:04:11 +0100

    feat(component): build a plugin to load components

---
+++ src/plugins/index.js
@@ -0,0 +1,12 @@
+import components from'./components'
+
+export default {
+  install: (app, options) => {
+    for (const prop in components) {
+      if (components.hasOwnProperty(prop)) {
+        const component = components[prop]
+        app.component(component.name, component)
+      }
+    }
+  }
+}
