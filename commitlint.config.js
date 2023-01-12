5615acc618029523e5b9c145af7f52fee42db61f
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-01-12 10:09:56 +0100

    feat: add changelog generator and new commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,30 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {},
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [1, "always", 100],
+    "header-max-length": [1, "always", 100],
+    "type-enum": [2, "always", ["delete", "security", "deprecate"]],
+  },
+  prompt: {
+    questions: {
+      type: {
+        enum: {
+          delete: {
+            description: "A features removed",
+            title: "Removed",
+          },
+          security: {
+            description: "Improves the security or resolves a security issue",
+            title: "Security",
+          },
+          deprecate: {
+            description:
+              "Deprecates existing functionality, but does not remove it",
+            title: "Deprecated",
+          },
+        },
+      },
+    },
+  },
 }
