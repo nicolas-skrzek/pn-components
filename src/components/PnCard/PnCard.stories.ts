1f044b0274c5dcfdb148688859b23f344cb582a0
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-01-25 11:50:33 +0100

    feat: add component card

---
+++ src/components/PnCard/PnCard.stories.ts
@@ -0,0 +1,29 @@
+import PnCard from "./PnCard.vue"
+export default {
+  title: "Nicolas Skrzek/UI Components/Card",
+  component: PnCard,
+}
+
+const Template = (args: any) => ({
+  components: { PnCard },
+  setup() {
+    return { args };
+  },
+  template: `
+    <pn-card>
+      <template #title>
+        Card title
+      </template>
+
+      <template #subtitle>
+        Card subtitle
+      </template>
+
+      <template #content>
+        <div>Card content</div>
+      </template>
+    </pn-card>
+  `
+})
+
+export const Default = Template.bind({})
---
+++ src/components/PnCard/PnCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PnCard',
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-header">
+      <slot name="header">
+        <div class="card-title">
+          <slot name="title" />
+        </div>
+        <div class="card-subtitle">
+          <slot name="subtitle" />
+        </div>
+      </slot>
+    </div>
+    <div class="card-content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
