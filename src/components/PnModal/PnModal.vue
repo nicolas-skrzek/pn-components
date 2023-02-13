73f5fcda6fba153eeb4f4b38a58daf6ee5718ad7
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-02-13 13:16:46 +0100

    feat: add modal component

---
+++ src/components/PnModal/PnModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PnCard from '@/components/PnCard/PnCard.vue';
+
+export default defineComponent({
+  name: 'PnModal',
+  components: { PnCard },
+})
+</script>
+
+<template>
+    <div class="pn-modal">
+      <div class="pn-overlay"></div>
+      <div class="pn-modal-content">
+        <slot name="content">
+          <pn-card>
+            <template #title>
+              <slot name="card-title" />
+            </template>
+
+            <template #subtitle>
+              <slot name="card-subtitle" />
+            </template>
+
+            <template #content>
+              <slot name="card-content" />
+            </template>
+          </pn-card>
+        </slot>
+      </div>
+    </div>
+</template>
