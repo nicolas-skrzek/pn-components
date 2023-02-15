9cec8618a43ee045a6217d650d3280a8711924ca
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-02-15 17:06:56 +0100

    fix: fix linter errors

---
+++ src/plugins/components.js
@@ -0,0 +1,7 @@
+import PnButton from '../components/pn-button/PnButton.vue'
+import PnTextField from '../components/pn-text-field/PnTextField.vue'
+
+export default {
+  PnButton,
+  PnTextField,
+}
