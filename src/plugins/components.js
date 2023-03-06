d2542a20cf3e78a7bc5d89b39d06d2cfd0a88b88
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-03-06 10:37:07 +0100

    refactor: fix types

--- src/plugins/components.js
+++
@@ -1,7 +0,0 @@
-import PnButton from '../components/pn-button/PnButton.vue'
-import PnTextField from '../components/pn-text-field/PnTextField.vue'
-
-export default {
-  PnButton,
-  PnTextField,
-}
