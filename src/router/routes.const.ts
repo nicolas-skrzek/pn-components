48280cd4b5855193a7f9458894066f17ee8b6ed0
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-05-10 17:39:16 +0200

    chore: remove storybook and add a custom sandbox

---
+++ src/router/routes.const.ts
@@ -0,0 +1,62 @@
+export const ROUTES = {
+  home: {
+    label: 'Home',
+    name: 'home',
+    path: '/',
+  },
+  button: {
+    label: 'Button',
+    name: 'button',
+    path: 'button',
+  },
+  calendar: {
+    label: 'Calendar',
+    name: 'calendar',
+    path: 'calendar',
+  },
+  card: {
+    label: 'Card',
+    name: 'card',
+    path: 'card',
+  },
+  list: {
+    label: 'List',
+    name: 'list',
+    path: 'list',
+  },
+  menu: {
+    label: 'Menu',
+    name: 'menu',
+    path: 'menu',
+  },
+  modal: {
+    label: 'Modal',
+    name: 'modal',
+    path: 'modal',
+  },
+  select: {
+    label: 'Select',
+    name: 'select',
+    path: 'select',
+  },
+  table: {
+    label: 'Table',
+    name: 'table',
+    path: 'table',
+  },
+  textarea: {
+    label: 'Textarea',
+    name: 'textarea',
+    path: 'textarea',
+  },
+  textfield: {
+    label: 'Textfield',
+    name: 'textfield',
+    path: 'textfield',
+  },
+  tree: {
+    label: 'Tree',
+    name: 'tree',
+    path: 'tree',
+  },
+}
---
+++ src/view/components/PnCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import PnCard from '@/components/PnCard/PnCard.vue'
+  import ControlsView from '@/view/controlsView.vue'
+</script>
+
+<template>
+  <controls-view>
+    <pn-card>
+      <template #title>
+        Card title
+      </template>
+
+      <template #subtitle>
+        Card subtitle
+      </template>
+
+      <template #content>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Curabitur pharetra lacus vel quam lobortis aliquam.
+          Vivamus fringilla felis sagittis nibh malesuada auctor.
+          Integer nec arcu risus. Nunc ornare eros vel scelerisque malesuada.
+          Curabitur at quam quis tortor pretium pretium non non lacus.
+        </p>
+      </template>
+    </pn-card>
+  </controls-view>
+</template>
