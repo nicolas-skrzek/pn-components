9904c506d501396be52458f7007177e0db5c6eb1
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-05-12 19:16:41 +0200

    feat(modal): add close action and size

--- src/components/PnCard/PnCard.vue
+++ src/components/PnCard/PnCard.vue
@@ -7,14 +7,19 @@ defineComponent({ name: 'PnCard' })
 <template>
   <div class="card">
     <div class="card-header">
-      <slot name="header">
-        <div class="card-title">
-          <slot name="title" />
-        </div>
-        <div class="card-subtitle">
-          <slot name="subtitle" />
-        </div>
-      </slot>
+      <div class="card-header-content">
+        <slot name="header">
+          <div class="card-title">
+            <slot name="title" />
+          </div>
+          <div class="card-subtitle">
+            <slot name="subtitle" />
+          </div>
+        </slot>
+      </div>
+      <div class="pn-card-action">
+        <slot name="action" />
+      </div>
     </div>
     <div class="card-content">
       <slot name="content" />
