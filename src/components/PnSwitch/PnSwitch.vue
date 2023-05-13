b56fa122ed182f3bcdd3a75ef7819526fb289071
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-05-13 22:11:35 +0200

    feat(switch): add component

---
+++ src/components/PnSwitch/PnSwitch.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { defineComponent, computed } from 'vue'
+import type { PnMode } from '@/types'
+
+export interface IPnTextField {
+  disabled?: boolean;
+  modelValue?: boolean;
+  mode?: PnMode;
+}
+
+defineComponent({
+  name: 'PnTextField',
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const props = withDefaults(defineProps<IPnTextField>(), {
+    disabled: false,
+    modelValue: false,
+    mode: 'light',
+})
+
+const checked = computed({
+  get: (): boolean => props.modelValue,
+  set: (value: boolean) => emits('update:modelValue', value),
+})
+
+const toggoleCheck = () => {
+  if (!props.disabled) {
+      checked.value = !checked.value
+  }
+}
+</script>
+
+<template>
+  <label class="pn-switch" :class="[mode, { disabled }]">
+    <div class="pn-switch-control" tabindex="0">
+      <div class="pn-switch-control-background" />
+      <div class="pn-switch-control-input" :class="{ checked }">
+        <input type="checkbox" tabindex="-1" :disabled="disabled" :checked="checked" @change="toggoleCheck()" />
+        <div class="pn-switch-control-action" />
+      </div>
+    </div>
+    <span class="pn-switch-label">
+      <slot />
+    </span>
+  </label>
+</template>
