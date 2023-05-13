6b52fa8195a5164171b9b51b2dd2256910e32275
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-05-13 21:06:37 +0200

    feat(checkbox): add component

---
+++ src/components/PnCheckbox/PnCheckbox.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { defineComponent, computed } from 'vue'
+import type { PnMode } from '@/types'
+
+export interface IPnTextField {
+  disabled?: boolean;
+  modelValue?: boolean;
+  mode?: PnMode;
+}
+
+defineComponent({
+  name: 'PnTextField',
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const props = withDefaults(defineProps<IPnTextField>(), {
+    disabled: false,
+    modelValue: false,
+    mode: 'light',
+})
+
+const checked = computed({
+  get: (): boolean => props.modelValue,
+  set: (value: boolean) => emits('update:modelValue', value),
+})
+
+const toggoleCheck = () => {
+  if (!props.disabled) {
+      checked.value = !checked.value
+  }
+}
+</script>
+
+<template>
+  <label class="pn-checkbox" tabindex="0" :class="[mode, { disabled }]" @click="toggoleCheck()" @keypress.space="toggoleCheck()">
+    <input type="checkbox" tabindex="-1" :disabled="disabled" :checked="checked" @change="toggoleCheck()" />
+    <div class="pn-checkbox-input" :class="{ checked }">
+      <div v-show="checked" class="pn-checkbox-inner" />
+    </div>
+    <span class="pn-checkbox-label">
+      <slot />
+    </span>
+  </label>
+</template>
