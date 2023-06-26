1c2cbce498d04e5c33db9c54c5b9b50292627ab2
Nicolas Skrzek <nicolas.skrzerk@gmail.com>
2023-06-26 15:50:52 +0200

    fix(menu): fix close where click outside the element

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import { clickOutside } from './utils/directives.utils'
 
 createApp(App)
   .use(router)
+  .directive('clickOutside', clickOutside)
   .mount('#app')
