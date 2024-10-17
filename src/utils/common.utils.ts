0114ed9cb6ceb76f8bb47c292e01d9a1a035f170
Nicolas Skrzek <nskrzek@mailoo.org>
2024-10-17 19:36:57 +0200

    refactor(table): change mechanic and style

--- src/utils/common.utils.ts
+++ src/utils/common.utils.ts
@@ -9,16 +9,3 @@ export const flattenArray = (array: any[]) => array.reduce((grp, item) => {
 
   return grp
 }, [])
-
-export const getKeysOfArrayObject = (array: any[]) => array.map((item) => Object.keys(item)).reduce((grp, item) => {
-  item.forEach((el) => {
-    const itemUpper = ucFirst(el)
-    const itemExist = grp.find((k) => k === itemUpper)
-
-    if (!itemExist) {
-      grp.push(itemUpper)
-    }
-  })
-
-  return grp
-}, [])
